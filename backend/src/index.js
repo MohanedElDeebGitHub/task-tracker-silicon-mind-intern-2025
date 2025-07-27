84243866cc271c0cf9f4461b485c57fd0b216bdf
MohanedElDieb <mohanedeldeebacc@gmail.com>
2025-07-27 08:59:53 +0300

    Fixed CORS, logging-in now works

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const authRoutes = require("./api/routes/auth.routes.js");
 const taskRoutes = require("./api/routes/task.routes.js");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
