dd93fafce62a68f2a61345a740df4fb433df0385
MohanedElDieb <mohanedeldeebacc@gmail.com>
2025-07-30 22:16:18 +0300

    swagger documentation and ui at 3001/api-docs

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ const authRoutes = require("./api/routes/auth.routes.js");
 const taskRoutes = require("./api/routes/task.routes.js");
 const morgan = require('morgan');
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express'); 
+const swaggerSpec = require('./config/swagger.config.js'); 
 
 const logger = require('./config/logger.js');
 
@@ -15,6 +17,9 @@ app.use(morgan('dev'));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // <-- ADD THIS
+
 
 const PORT = process.env.PORT || 3001;
 
